4570fb11eb3102cfa68a26b29264ad2418287519
Yufei Gu <81865869+Yufei-Gu-451@users.noreply.github.com>
2022-03-08 17:38:02 +0000

    Merge NLP Processor into code

---
+++ app/main/nlp.py
@@ -0,0 +1,8 @@
+import speech_to_text
+import text_summarizer
+
+#speech_to_text.speech_to_text(inputfile="AUDIO/aboutSpeechSdk.wav", 
+#                outputfile="FILE/temp_result.txt")
+
+text_summarizer.summarize_text(input_file='FILE/temp_input.txt', 
+            output_file='FILE/temp_output.txt', compression_rate=0.3, number_of_clusters=4)
